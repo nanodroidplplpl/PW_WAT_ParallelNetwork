356ae7843f1eae7bf9e0d77f4b649235a2f15c93
nanodroidplplpl <52038211+nanodroidplplpl@users.noreply.github.com>
2022-01-23 14:11:33 +0100

    Update StacjaRobocza.java

--- src/siec/StacjaRobocza.java
+++ src/siec/StacjaRobocza.java
@@ -20,18 +20,20 @@ public class StacjaRobocza extends Thread {
     }
 
     Random r = new Random();
+    Random R = new Random();
     public void run() {
         for ( int i = 1; i <= N; i++ ) {
             try {
                 rr = r.nextInt(2);
-                LR = server.uzyskaj_dostep(getName(), rr, grupa, i); // String nazwa, int co_bierzesz, int gr, int nr_powt
+                LR = server.uzyskaj_dostep(getName(), rr, grupa, i, numer); // String nazwa, int co_bierzesz, int gr, int nr_powt
                 // LR jezeli 0 to lewy jezeli 1 to prawy
-                sleep(r.nextInt(5));
+                sleep(R.nextInt(5));
                 //System.out.println("Moje urzo to "+LR+" jestem "+getName());
-                server.zwolnij_zasob(getName(), rr, grupa, i, LR); // String nazwa, int co_bierzesz, int gr, int nr_powt, int ktore
+                server.zwolnij_zasob(getName(), rr, grupa, i, LR, numer); // String nazwa, int co_bierzesz, int gr, int nr_powt, int ktore
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 }
+
